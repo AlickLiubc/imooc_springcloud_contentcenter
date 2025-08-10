6bf528550400920690ede9e06c24201d5e48f3d7
AlickLiubc <nrlbc@sina.com>
2025-08-10 21:36:25 +0800

    权限验证模块

---
+++ src/main/java/com/itmuch/contentcenter/feignclient/interceptor/TokenRelayRequestInterceptor.java
@@ -0,0 +1,23 @@
+package com.itmuch.contentcenter.feignclient.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TokenRelayRequestInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) requestAttributes;
+        HttpServletRequest httpServletRequest = attributes.getRequest();
+
+        String token = httpServletRequest.getHeader("X-Token");
+        requestTemplate.header("X-Token", token);
+    }
+
+}
